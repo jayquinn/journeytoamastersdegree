68cc4611a27d189c7368a0ebb482bcd036736d3c
Hyeon-jong Jay Yu <61223949+jayquinn@users.noreply.github.com>
2021-11-15 23:40:34 +0900

    asdfasfsa

--- preprocessing.R
+++ preprocessing.R
@@ -3,6 +3,7 @@ library(mirt)
 library(psych) # phi 계수 https://www.r-bloggers.com/2021/07/how-to-calculate-phi-coefficient-in-r/
 library(lavaan) #CFA
 library("vcd") # kappa
+library(caret)
 # cohen.kappa 랑 wkappa ?cohen.kappa
 # mirt simdata
 # psych https://personality-project.org/r/html/sim.html
